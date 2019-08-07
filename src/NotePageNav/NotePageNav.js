98c26152ccdb34b529ff1e842f7bacfbf7b8154c
m-cal <iengineersoftware@gmail.com>
2019-08-07 15:13:25 -0400

    making progress with context conversion

--- src/NotePageNav/NotePageNav.js
+++ src/NotePageNav/NotePageNav.js
@@ -2,31 +2,31 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import CircleButton from '../CircleButton/CircleButton'
 import './NotePageNav.css'
+import NoteContext from '../NoteContext';
 
-export default function NotePageNav(props) {
-  return (
-    <div className='NotePageNav'>
-      <CircleButton
-        tag='button'
-        role='link'
-        onClick={() => props.history.goBack()}
-        className='NotePageNav__back-button'
-      >
-        <FontAwesomeIcon icon='chevron-left' />
-        <br />
-        Back
-      </CircleButton>
-      {props.folder && (
-        <h3 className='NotePageNav__folder-name'>
-          {props.folder.name}
-        </h3>
-      )}
-    </div>
-  )
-}
-
-NotePageNav.defaultProps = {
-  history: {
-    goBack: () => {}
+class NotePageNav extends React.Component{
+  static contextType = NoteContext;
+  render() {
+    return (
+      <div className='NotePageNav'>
+        <CircleButton
+          tag='button'
+          role='link'
+          onClick={() => this.context.history.goBack()}
+          className='NotePageNav__back-button'
+        >
+          <FontAwesomeIcon icon='chevron-left' />
+          <br />
+          Back
+        </CircleButton>
+        {this.context.folder && (
+          <h3 className='NotePageNav__folder-name'>
+            {this.context.folder.name}
+          </h3>
+        )}
+      </div>
+    )
   }
 }
+
+export default NotePageNav;
