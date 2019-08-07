5c65720597d943033f4ccfc60f5b528537c02b8e
m-cal <iengineersoftware@gmail.com>
2019-08-07 15:56:07 -0400

    context conversion progress continues

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -3,29 +3,35 @@ import { Link } from 'react-router-dom'
 import { format } from 'date-fns'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Note.css'
+import NoteContext from '../NoteContext'
 
-export default function Note(props) {
-  return (
-    <div className='Note'>
-      <h2 className='Note__title'>
-        <Link to={`/note/${props.id}`}>
-          {props.name}
-        </Link>
-      </h2>
-      <button className='Note__delete' type='button'>
-        <FontAwesomeIcon icon='trash-alt' />
-        {' '}
-        remove
-      </button>
-      <div className='Note__dates'>
-        <div className='Note__dates-modified'>
-          Modified
+class Note extends React.Component{
+  static contextType = NoteContext;
+  render() {
+    return (
+      <div className='Note'>
+        <h2 className='Note__title'>
+          <Link to={`/note/${this.context.id}`}>
+            {this.context.name}
+          </Link>
+        </h2>
+        <button className='Note__delete' type='button'>
+          <FontAwesomeIcon icon='trash-alt' />
           {' '}
-          <span className='Date'>
-            {format(props.modified, 'Do MMM YYYY')}
-          </span>
+          remove
+        </button>
+        <div className='Note__dates'>
+          <div className='Note__dates-modified'>
+            Modified
+            {' '}
+            <span className='Date'>
+              {format(this.context.modified, 'Do MMM YYYY')}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
+
+export default Note;
